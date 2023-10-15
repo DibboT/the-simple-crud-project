a5eb2ea4ec53a743de51c1aafa9db41a84c59442
Dibbo T <dibbotripura7@gmail.com>
2023-10-15 23:53:40 +0600

    the-simple-crud-project created

---
+++ simple-crud-client/src/App.jsx
@@ -0,0 +1,22 @@
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <form>
+        <input type="text" name="name" id=""/>
+        <br/>
+        <input type="email" name="email" id=""/>
+        <br/>
+        <input type="submit" value="Add User"/>
+      </form>
+      
+    </>
+  )
+}
+
+export default App
