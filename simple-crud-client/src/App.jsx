e27a40d565c4fc60fce3643a8a78481029e6180c
Dibbo T <dibbotripura7@gmail.com>
2023-10-16 11:44:40 +0600

    first commit

--- simple-crud-client/src/App.jsx
+++ simple-crud-client/src/App.jsx
@@ -3,11 +3,32 @@
 import './App.css'
 
 function App() {
+  const handleAddUser = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.name.value;
+    const user = {name, email};
+    console.log(user);
+
+    fetch('http://localhost:5000/users', {
+      method: 'POST' ,
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    })
+  }
 
   return (
     <>
       <h1>Vite + React</h1>
-      <form>
+      <form onSubmit={handleAddUser}>
         <input type="text" name="name" id=""/>
         <br/>
         <input type="email" name="email" id=""/>
--- simple-crud-server/index.js
+++ simple-crud-server/index.js
@@ -7,11 +7,14 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
-require('dotenv').config();
+// require('dotenv').config();
+
+//simpleCrud
+// /nqU0q9xHaII5FXoF
 
 
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.igadn9s.mongodb.net/?retryWrites=true&w=majority";
-console.log(uri)
+
+const uri = "mongodb+srv://simpleCrud:nqU0q9xHaII5FXoF@cluster0.igadn9s.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,6 +29,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("usersDB");
+    const userCollection = database.collection("users");
+
+    app.post('/users', async(req, res) =>{
+      const user = req.body;
+      console.log(user)
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    } )
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
